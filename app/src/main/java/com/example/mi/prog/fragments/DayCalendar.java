bf1ac8e3ed4b0cc9b06fef5b73907ceb5aa94930
Colon1al <38556136+Colon1al@users.noreply.github.com>
2018-09-12 11:55:51 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml

--- app/src/main/java/com/example/mi/prog/fragments/DayCalendar.java
+++ app/src/main/java/com/example/mi/prog/fragments/DayCalendar.java
@@ -85,21 +85,10 @@ public class DayCalendar extends Fragment {
             @Override
             public void onClick(View v) {
                 LISTDATA l = new LISTDATA();
-               // l.clearFile(getActivity());
             }
         });
+    }
 
-    }//TODO: говнокод
-    /*@Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
 
     @Override
     public void onDetach() {
--- app/src/main/java/com/example/mi/prog/fragments/testfragment.java
+++ app/src/main/java/com/example/mi/prog/fragments/testfragment.java
@@ -123,19 +123,6 @@ public class testfragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
-
-    //TODO:govnocode
-    /*@Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }*/
-
     @Override
     public void onDetach() {
         super.onDetach();
